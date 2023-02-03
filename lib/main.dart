7b584b333f53d69b159fd4b607ffa7cdf85773f9
nohsangwoo <nsgr12@naver.com>
2023-02-03 09:47:31 +0900

    Cards section

--- lib/main.dart
+++ lib/main.dart
@@ -33,87 +33,170 @@ class App extends StatelessWidget {
       backgroundColor: const Color(0xFF181818),
       // 위젯 위에 다른 위젯을 올리고 싶으면 Column을 사용해야한다.
       body: Padding(
-          // EdgeInsets 패딩을 설정하는 메소드
-          // EdgeInsets.all(10) 싱히좌우 모두 10의 패딩을 준다.
-          // EdgeInsets.only(top:20) 상단 부분만 20의 패딩을 준다.
-          // EdgeInsets.symmetric(vertical: 30) 상하 부분만 30의 패딩을 준다.
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              Row(
-                // Row에사 수평 정렬 방식(flex랑 비슷함)
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    // Column에서 수평 정렬방식
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        "Hey, Selena",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800,
-                        ),
+        // EdgeInsets 패딩을 설정하는 메소드
+        // EdgeInsets.all(10) 싱히좌우 모두 10의 패딩을 준다.
+        // EdgeInsets.only(top:20) 상단 부분만 20의 패딩을 준다.
+        // EdgeInsets.symmetric(vertical: 30) 상하 부분만 30의 패딩을 준다.
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            Row(
+              // Row에사 수평 정렬 방식(flex랑 비슷함)
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  // Column에서 수평 정렬방식
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      "Hey, Selena",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
                       ),
-                      Text(
-                        "Welcome back",
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontSize: 18,
-                        ),
+                    ),
+                    Text(
+                      "Welcome back",
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 120,
+            ),
+            Text(
+              "Total Balance",
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white.withOpacity(0.8),
               ),
-              const SizedBox(
-                height: 120,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              "\$5 194 482",
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
               ),
-              Text(
-                "Total Balance",
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white.withOpacity(0.8),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Button(
+                  text: "Transfer",
+                  bgColor: Color(0xFF1F2123),
+                  textColor: Colors.black,
                 ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const Text(
-                "\$5 194 482",
-                style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
+                Button(
+                  text: "Request",
+                  bgColor: Color(0xFF1F2123),
+                  textColor: Colors.white,
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Button(
-                    text: "Transfer",
-                    bgColor: Color(0xFF1F2123),
-                    textColor: Colors.black,
+              ],
+            ),
+            const SizedBox(
+              height: 100,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const Text(
+                  "Wallets",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.w600,
                   ),
-                  Button(
-                    text: "Request",
-                    bgColor: Color(0xFF1F2123),
-                    textColor: Colors.white,
-                  )
-                ],
-              )
-            ],
-          )),
+                ),
+                Text(
+                  "View All",
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFF1F2123),
+                borderRadius: BorderRadius.circular(
+                  25,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  30,
+                ),
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Euro",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            const Text(
+                              "6 428",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "EUR",
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(
+                                  0.8,
+                                ),
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     ));
   }
 }
